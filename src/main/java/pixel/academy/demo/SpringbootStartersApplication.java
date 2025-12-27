2eabdf8cf843fd5a2d68784634ae625676bd9be3
Pusca Vasile <vasea-pusca@mail.ru>
2025-12-27 10:55:21 +0200

    Initial comit

---
+++ src/main/java/pixel/academy/demo/SpringbootStartersApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStartersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStartersApplication.class, args);
+	}
+
+}
